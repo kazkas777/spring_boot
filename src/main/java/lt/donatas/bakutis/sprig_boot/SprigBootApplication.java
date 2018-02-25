8c9bf0a21322136a6aa3a5c3de892e616451e867
kazkas777 <kazkas777@gmail.com>
2018-02-25 17:16:04 +0200

    first commit

---
+++ src/main/java/lt/donatas/bakutis/sprig_boot/SprigBootApplication.java
@@ -0,0 +1,12 @@
+package lt.donatas.bakutis.sprig_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigBootApplication.class, args);
+	}
+}
